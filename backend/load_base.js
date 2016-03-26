7319c5fbdefbc44ab9d2bc54e9a43ebaafe56aa9
FedeG <federicog@exo.com.ar>
2016-03-26 06:35:49 -0300

    Datos de talles + nuevas fotos

--- backend/load_base.js
+++ backend/load_base.js
@@ -18,37 +18,37 @@ var contactos = [
 ];
 
 var ambos = [
-  {'name': 'ambo1', 'codigo': 'a1', 'tipo': 'ambo', 'imagen_completa': 'static/img/portfolio/ambo1.jpeg', 'imagen': ['static/img/portfolio/ambo1.jpeg'], 'precio': 800, 'stock': 0},
-  {'name': 'ambo2', 'codigo': 'a2', 'tipo': 'ambo', 'imagen_completa': 'static/img/portfolio/ambo2.jpg', 'imagen': ['static/img/portfolio/ambo2.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'ambo3', 'codigo': 'a3', 'tipo': 'ambo', 'imagen_completa': 'static/img/portfolio/ambo3.jpg', 'imagen': ['static/img/portfolio/ambo3.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'ambo4', 'codigo': 'a4', 'tipo': 'ambo', 'imagen_completa': 'static/img/portfolio/ambo4.jpg', 'imagen': ['static/img/portfolio/ambo4.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'ambo5', 'codigo': 'a5', 'tipo': 'ambo', 'imagen_completa': 'static/img/portfolio/ambo5.jpg', 'imagen': ['static/img/portfolio/ambo5.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'ambo6', 'codigo': 'a6', 'tipo': 'ambo', 'imagen_completa': 'static/img/portfolio/ambo6.jpg', 'imagen': ['static/img/portfolio/ambo6.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'ambo7', 'codigo': 'a7', 'tipo': 'ambo', 'imagen_completa': 'static/img/portfolio/ambo7.jpg', 'imagen': ['static/img/portfolio/ambo7.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'ambo8', 'codigo': 'a8', 'tipo': 'ambo', 'imagen_completa': 'static/img/portfolio/ambo8.jpg', 'imagen': ['static/img/portfolio/ambo8.jpg'], 'precio': 800, 'stock': 0}
+  {'name': 'Victory Flex', 'codigo': 'vf', 'tipo': 'ambo', 'imagen_completa': 'static/img/ambos/VictoriFlex.jpeg', 'imagen': ['static/img/ambos/VictoriFlex.jpeg', 'static/img/ambos/VictoryFlex.jpeg','static/img/ambos/VictoryFlex2.jpeg','static/img/ambos/VictoryFlex3.jpeg', 'static/img/ambos/VictoryFlex4.jpeg','static/img/ambos/VictoryFlex5.jpeg'], 'precio': 800, 'stock': 0},
+  {'name': 'Victory Print', 'codigo': 'vp', 'tipo': 'ambo', 'imagen_completa': 'static/img/ambos/VictoryPrint.jpeg', 'imagen': ['static/img/ambos/VictoryPrint.jpeg', 'static/img/ambos/VictoryPrint1.jpeg', 'static/img/ambos/VictoryPrint2.jpeg', 'static/img/ambos/VictoryPrint3.jpeg', 'static/img/ambos/VictoryPrint4.jpeg', 'static/img/ambos/VictoryPrint5.jpeg'], 'precio': 750, 'stock': 0},
+  {'name': 'Victory Sea', 'codigo': 'vse', 'tipo': 'ambo', 'imagen_completa': 'static/img/ambos/VictorySea.jpeg', 'imagen': ['static/img/ambos/VictorySea.jpeg', 'static/img/ambos/VictorySea1.jpeg', 'static/img/ambos/VictorySea2.jpeg', 'static/img/ambos/VictorySea3.jpeg', 'static/img/ambos/VictorySea4.jpeg', 'static/img/ambos/VictorySea5.jpeg'], 'precio': 750, 'stock': 0},
+  {'name': 'Victory Spring', 'codigo': 'vsp', 'tipo': 'ambo', 'imagen_completa': 'static/img/ambos/VictorySpring.jpeg', 'imagen': ['static/img/ambos/VictorySpring.jpeg', 'static/img/ambos/VictorySpring1.jpeg', 'static/img/ambos/VictorySpring2.jpeg', 'static/img/ambos/VictorySprint3.jpeg', 'static/img/ambos/VictorySpring4.jpeg', 'static/img/ambos/VictorySpring5.jpeg'], 'precio': 750, 'stock': 0},
+  {'name': 'Victory Sweet', 'codigo': 'vsw', 'tipo': 'ambo', 'imagen_completa': 'static/img/ambos/Victory sweet.jpeg', 'imagen': ['static/img/ambos/Victory sweet.jpeg', 'static/img/ambos/VictorySweet.jpeg', 'static/img/ambos/VictorySweet2.jpeg', 'static/img/ambos/VictorySweet3.jpeg', 'static/img/ambos/VictorySweet4.jpeg', 'static/img/ambos/VictorySweet5.jpeg'], 'precio': 750, 'stock': 0}
 ];
 
 var chaquetas = [
-  {'name': 'chaqueta1', 'codigo': 'c1', 'tipo': 'chaqueta', 'imagen_completa': 'static/img/portfolio/chaquetas1.jpeg', 'imagen': ['static/img/portfolio/chaquetas1.jpeg'], 'precio': 800, 'stock': 0},
-  {'name': 'chaqueta2', 'codigo': 'c2', 'tipo': 'chaqueta', 'imagen_completa': 'static/img/portfolio/chaquetas2.jpeg', 'imagen': ['static/img/portfolio/chaquetas2.jpeg'], 'precio': 800, 'stock': 0},
-  {'name': 'chaqueta3', 'codigo': 'c3', 'tipo': 'chaqueta', 'imagen_completa': 'static/img/portfolio/chaquetas3.jpg', 'imagen': ['static/img/portfolio/chaquetas3.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'chaqueta4', 'codigo': 'c4', 'tipo': 'chaqueta', 'imagen_completa': 'static/img/portfolio/chaquetas4.jpg', 'imagen': ['static/img/portfolio/chaquetas4.jpg'], 'precio': 800, 'stock': 0}
+  {'name': 'Victory Flex', 'codigo': 'vfc', 'tipo': 'chaqueta', 'imagen_completa': 'static/img/ambos/VictoryFlex4.jpeg', 'imagen': ['static/img/ambos/VictoryFlex4.jpeg','static/img/ambos/VictoryFlex5.jpeg'], 'precio': 800, 'stock': 0},
+  {'name': 'Victory Print', 'codigo': 'vpc', 'tipo': 'chaqueta', 'imagen_completa': 'static/img/ambos/VictoryPrint2.jpeg', 'imagen': ['static/img/ambos/VictoryPrint2.jpeg', 'static/img/ambos/VictoryPrint1.jpeg'], 'precio': 750, 'stock': 0},
+  {'name': 'Victory Sea', 'codigo': 'vsec', 'tipo': 'chaqueta', 'imagen_completa': 'static/img/ambos/VictorySea5.jpeg', 'imagen': ['static/img/ambos/VictorySea5.jpeg', 'static/img/ambos/VictorySea4.jpeg', 'static/img/ambos/VictorySea1.jpeg'], 'precio': 750, 'stock': 0},
+  {'name': 'Victory Spring', 'codigo': 'vspc', 'tipo': 'chaqueta', 'imagen_completa': 'static/img/ambos/VictorySpring2.jpeg', 'imagen': ['static/img/ambos/VictorySpring2.jpeg', 'static/img/ambos/VictorySpring1.jpeg'], 'precio': 750, 'stock': 0},
+  {'name': 'Victory Sweet', 'codigo': 'vswc', 'tipo': 'chaqueta', 'imagen_completa': 'static/img/ambos/VictorySweet2.jpeg', 'imagen': ['static/img/ambos/VictorySweet2.jpeg', 'static/img/ambos/VictorySweet.jpeg'], 'precio': 750, 'stock': 0}
 ];
 
 var pantalones = [
-  {'name': 'pantalon1', 'codigo': 'p1', 'tipo': 'pantalon', 'imagen_completa': 'static/img/portfolio/pantalon1.jpg', 'imagen': ['static/img/portfolio/pantalon1.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'pantalon2', 'codigo': 'p2', 'tipo': 'pantalon', 'imagen_completa': 'static/img/portfolio/pantalon2.jpg', 'imagen': ['static/img/portfolio/pantalon2.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'pantalon3', 'codigo': 'p3', 'tipo': 'pantalon', 'imagen_completa': 'static/img/portfolio/pantalon1.jpg', 'imagen': ['static/img/portfolio/pantalon1.jpg'], 'precio': 800, 'stock': 0},
-  {'name': 'pantalon4', 'codigo': 'p4', 'tipo': 'pantalon', 'imagen_completa': 'static/img/portfolio/pantalon2.jpg', 'imagen': ['static/img/portfolio/pantalon2.jpg'], 'precio': 800, 'stock': 0}
+  {'name': 'Victory Flex', 'codigo': 'vfp', 'tipo': 'pantalon', 'imagen_completa': 'static/img/ambos/VictoryFlex3.jpeg', 'imagen': ['static/img/ambos/VictoryFlex3.jpeg', 'static/img/ambos/VictoryFlex.jpeg','static/img/ambos/VictoryFlex2.jpeg'], 'precio': 800, 'stock': 0},
+  {'name': 'Victory Print', 'codigo': 'vpp', 'tipo': 'pantalon', 'imagen_completa': 'static/img/ambos/VictoryPrint5.jpeg', 'imagen': ['static/img/ambos/VictoryPrint5.jpeg', 'static/img/ambos/VictoryPrint3.jpeg', 'static/img/ambos/VictoryPrint4.jpeg'], 'precio': 750, 'stock': 0},
+  {'name': 'Victory Sea', 'codigo': 'vsep', 'tipo': 'pantalon', 'imagen_completa': 'static/img/ambos/VictorySea3.jpeg', 'imagen': ['static/img/ambos/VictorySea3.jpeg', 'static/img/ambos/VictorySea2.jpeg'], 'precio': 750, 'stock': 0},
+  {'name': 'Victory Spring', 'codigo': 'vspp', 'tipo': 'pantalon', 'imagen_completa': 'static/img/ambos/VictorySprint3.jpeg', 'imagen': ['static/img/ambos/VictorySprint3.jpeg', 'static/img/ambos/VictorySpring4.jpeg', 'static/img/ambos/VictorySpring5.jpeg'], 'precio': 750, 'stock': 0},
+  {'name': 'Victory Sweet', 'codigo': 'vswp', 'tipo': 'pantalon', 'imagen_completa': 'static/img/ambos/VictorySweet3.jpeg', 'imagen': ['static/img/ambos/VictorySweet3.jpeg', 'static/img/ambos/VictorySweet4.jpeg', 'static/img/ambos/VictorySweet5.jpeg'], 'precio': 750, 'stock': 0}
 ];
 
 var talles = [
-  {'size': 'xs', 'busto': '10 cm', 'alto': '5 cm'},
-  {'size': 's', 'busto': '15 cm', 'alto': '10 cm'},
-  {'size': 'm', 'busto': '20 cm', 'alto': '10 cm'},
-  {'size': 'l', 'busto': '25 cm', 'alto': '20 cm'},
-  {'size': 'xl', 'busto': '30 cm', 'alto': '21 cm'},
-  {'size': 'xxl', 'busto': '35 cm', 'alto': '25 cm'}
+  {'size': 'xs', 'hombros': '40 cm', 'busto': '92 cm', 'largo_c': '62 cm', 'cadera': '88 cm', 'largo_p': '93 cm'},
+  {'size': 's', 'hombros': '41 cm', 'busto': '96 cm', 'largo_c': '64 cm', 'cadera': '92 cm', 'largo_p': '96 cm'},
+  {'size': 'm', 'hombros': '42 cm', 'busto': '100 cm', 'largo_c': '66 cm', 'cadera': '95 cm', 'largo_p': '100 cm'},
+  {'size': 'l', 'hombros': '43 cm', 'busto': '104 cm', 'largo_c': '68 cm', 'cadera': '95 cm', 'largo_p': '108 cm'},
+  {'size': 'xl', 'hombros': '44 cm', 'busto': '110 cm', 'largo_c': '72 cm', 'cadera': '104 cm', 'largo_p': '110 cm'},
+  {'size': '8', 'hombros': '45 cm', 'busto': '120 cm', 'largo_c': '77 cm', 'cadera': '112 cm', 'largo_p': '114 cm'},
+  {'size': '10', 'hombros': '46 cm', 'busto': '132 cm', 'largo_c': '78 cm', 'cadera': '127 cm', 'largo_p': '114 cm'}
 ];
 
 load('usuario');
